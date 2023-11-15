83cc159280b295a61904be5eabbedc3edd5c22cd
myntp <119732035+MYNTCODE@users.noreply.github.com>
2023-11-15 18:57:52 +0700

    feat: add project

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Education from "./pages/Education";
 import NotFoundPage from "./pages/404";
+import HomeService from "./pages/HomeService";
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/education" element={<Education />} />
+      <Route path="/homeservice" element={<HomeService />} />
       <Route path="*" element={<NotFoundPage />}></Route>
     </Routes>
   );
