c274c3f1335230b7eec00a41d6965e1967653881
myntp <119732035+MYNTCODE@users.noreply.github.com>
2023-11-16 00:02:07 +0700

    edit router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+// import "./style/Homeservice.css";
+// import { BrowserRouter } from "react-router-dom";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./index.css";
 import "./style/Homeservice.css";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+const root = document.getElementById("root");
+const rootElement = ReactDOM.createRoot(root);
+
+rootElement.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
