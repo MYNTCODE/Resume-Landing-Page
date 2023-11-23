f15b59a36dda12fe82a136c07b912911c83eb29e
myntp <119732035+MYNTCODE@users.noreply.github.com>
2023-11-23 20:20:31 +0700

    feat: add contact section can use POST API

---
+++ src/components/ContactBox.jsx
@@ -0,0 +1,137 @@
+import { useState } from "react";
+import { message, Button, Upload } from "antd";
+import axios from "axios";
+
+function ContactBox() {
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userMessage, setUserMessage] = useState("");
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      setIsError(false);
+      setIsLoading(true);
+
+      const response = await axios.post(
+        "https://backend-api-deploy.vercel.app/api/v1/users",
+        {
+          name: userName,
+          email: userEmail,
+          message: userMessage,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          timeout: 5000,
+        }
+      );
+
+      setIsLoading(false);
+
+      if (response.status >= 200 && response.status < 300) {
+        // Handle success
+        setUserName("");
+        setUserEmail("");
+        setUserMessage("");
+        message.success("I appreciate you reaching out");
+      } else {
+        // Handle other status codes (e.g., show an error message)
+        console.error("Unsuccessful response:", response);
+        message.error("Failed to submit the form");
+      }
+    } catch (error) {
+      setIsError(true);
+      setIsLoading(false);
+      console.error("Error submitting the form:", error);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    switch (name) {
+      case "name":
+        setUserName(value);
+        break;
+      case "email":
+        setUserEmail(value);
+        break;
+      case "message":
+        setUserMessage(value);
+        break;
+      default:
+        break;
+    }
+  };
+  return (
+    <>
+      <div className="text-white flex-col w-[300px] lg:w-[500px] mt-4  lg:my-4 lg:p-10">
+        <div className="contacts-box ">
+          <form onSubmit={handleSubmit}>
+            <div className="ml-[-25px] flex justify-center lg:gap-5">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={userName}
+                onChange={handleChange}
+                className="text-black p-2 rounded-xl ml-2 mb-10 w-[200px] lg:w-[300px] h-[50px]"
+                required
+              />
+            </div>
+            <div className="ml-[-30px] flex justify-center lg:gap-5">
+              <label htmlFor="email ">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={userEmail}
+                onChange={handleChange}
+                className="text-black p-2 rounded-xl ml-2 mb-10 w-[200px] lg:w-[300px] h-[50px]"
+                required
+              />
+            </div>
+            <div className=" flex lg:justify-center lg:ml-[-45px] lg:gap-5">
+              <label htmlFor="message" className=" pr-2">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                value={userMessage}
+                onChange={handleChange}
+                required
+                style={{ resize: "none" }}
+                className="text-black p-2 rounded-xl mb-8 w-[200px] lg:w-[300px] h-[100px]"
+              ></textarea>
+            </div>
+
+            <button
+              type="submit"
+              className="w-[20%] lg:ml-10 h-[50px] border rounded-xl "
+              disabled={isLoading}
+            >
+              {isLoading ? "Sending..." : "Send"}
+            </button>
+          </form>
+          {isError && (
+            <p className="error-message">
+              There was an error submitting the form. Please try again.
+            </p>
+          )}
+          {successMessage && (
+            <p className="success-message">{successMessage}</p>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ContactBox;
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import ContactBox from "../components/ContactBox";
 
 const Link = (props) => {
   return (
@@ -16,38 +17,48 @@ Link.propTypes = {
 const ContactPage = () => {
   return (
     <>
-      <div className="contact-container px-20 h-[30vh] lg:h-[40vh] lg:border-black bg-black text-center pt-6 lg:pt-4">
-        <h1 className=" text-[30px] lg:text-[50px] lg:mt-[4%] text-white">
+      <div className="contact-container px-20 h-[70vh] lg:h-[60vh] lg:border-black bg-black text-center pt-6 lg:pt-12 ">
+        <h1 className=" lg:hidden text-[30px] lg:text-[50px] lg:mt-[4%] text-white">
           Contact
         </h1>
-        <div className="contact-icon flex justify-center mt-8 lg:mt-0 space-x-6 lg:pt-6 ">
-          <Link url="https://github.com/MYNTCODE">
-            <img
-              src="https://cdn.discordapp.com/attachments/1144637676016898219/1169301599320023152/github_4.png?ex=6554e7f9&is=654272f9&hm=08c6e9c22ccdc84ab86f48a265a87ae637706c55191c6b7c7cf2f71662bc2f61&"
-              alt="Github"
-            />
-          </Link>
+        <div className=" lg:flex lg:justify-center lg:item-center lg:gap-10  ">
+          <div className="ml-[-39px]">
+            <ContactBox />
+          </div>
+          <div>
+            <h1 className="hidden lg:block text-[30px] lg:text-[50px] lg:mt-[10%] text-white">
+              Contact Me !
+            </h1>
+            <div className="contact-icon flex justify-center items-center mt-8 lg:mt-[40px] space-x-6">
+              <Link url="https://github.com/MYNTCODE">
+                <img
+                  src="https://cdn.discordapp.com/attachments/1144637676016898219/1169301599320023152/github_4.png?ex=6554e7f9&is=654272f9&hm=08c6e9c22ccdc84ab86f48a265a87ae637706c55191c6b7c7cf2f71662bc2f61&"
+                  alt="Github"
+                />
+              </Link>
 
-          <Link url="https://www.linkedin.com/in/pornphimol-saelao/">
-            <img
-              src="https://cdn.discordapp.com/attachments/1144637676016898219/1169302121779314821/linkedin_1.png?ex=6554e876&is=65427376&hm=e17fac1627590b9bcbb0788f6aae3c6f1794cead96c5691580103d2fdfa24a4b&"
-              alt="LinkedIn"
-            />
-          </Link>
+              <Link url="https://www.linkedin.com/in/pornphimol-saelao/">
+                <img
+                  src="https://cdn.discordapp.com/attachments/1144637676016898219/1169302121779314821/linkedin_1.png?ex=6554e876&is=65427376&hm=e17fac1627590b9bcbb0788f6aae3c6f1794cead96c5691580103d2fdfa24a4b&"
+                  alt="LinkedIn"
+                />
+              </Link>
 
-          <Link url="https://www.instagram.com/mynt_p/?hl=en">
-            <img
-              src="https://cdn.discordapp.com/attachments/1144637676016898219/1169302340495491124/instagram_4.png?ex=6554e8aa&is=654273aa&hm=022fb8895075b9aa51d926b78e5ee7ffe31b2763b71f2462f5ff86ea40c10248&"
-              alt="Instagram"
-            />
-          </Link>
+              <Link url="https://www.instagram.com/mynt_p/?hl=en">
+                <img
+                  src="https://cdn.discordapp.com/attachments/1144637676016898219/1169302340495491124/instagram_4.png?ex=6554e8aa&is=654273aa&hm=022fb8895075b9aa51d926b78e5ee7ffe31b2763b71f2462f5ff86ea40c10248&"
+                  alt="Instagram"
+                />
+              </Link>
 
-          <Link url="mailto:myntpwork@gmail.com">
-            <img
-              src="https://cdn.discordapp.com/attachments/1144637676016898219/1169302644876120144/gmail-logo.png?ex=6554e8f3&is=654273f3&hm=786a5fdad540770c019b7bae59036eaac0ad15c749cbd78dbccb6d1b943c1fbe&"
-              alt="Email"
-            />
-          </Link>
+              <Link url="mailto:myntpwork@gmail.com">
+                <img
+                  src="https://cdn.discordapp.com/attachments/1144637676016898219/1169302644876120144/gmail-logo.png?ex=6554e8f3&is=654273f3&hm=786a5fdad540770c019b7bae59036eaac0ad15c749cbd78dbccb6d1b943c1fbe&"
+                  alt="Email"
+                />
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </>
