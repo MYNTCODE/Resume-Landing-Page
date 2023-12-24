9143b21aecad4fead7c0aebbf8b27ff2dbf1bad8
myntp <119732035+MYNTCODE@users.noreply.github.com>
2023-12-24 15:17:22 +0700

    Update new project

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -19,11 +19,17 @@ const projects = [
       "https://cdn.discordapp.com/attachments/1144637676016898219/1163529172359061565/Screenshot_2023-10-17_001729.jpg?ex=653fe7fc&is=652d72fc&hm=cd4665d5649474c634479797e7305cacadf0546d0a9f9f11e3c729e002811f2c&",
     link: "https://agents-abilities-data.vercel.app/",
   },
+  // {
+  //   title: "Product Page (Responsive)",
+  //   image:
+  //     "https://cdn.discordapp.com/attachments/1144637676016898219/1166813394301628476/Screenshot_2023-10-26_015936.jpg?ex=654bdaa7&is=653965a7&hm=1eb461e049c0f360a79e22ba9238b261cbcb5d604597e432e48bbc45891778c7&",
+  //   link: "https://clothes-shopping-page-responsive.vercel.app/",
+  // },
   {
-    title: "Product Page (Responsive)",
+    title: "Fashion Landing Page (Responsive)",
     image:
-      "https://cdn.discordapp.com/attachments/1144637676016898219/1166813394301628476/Screenshot_2023-10-26_015936.jpg?ex=654bdaa7&is=653965a7&hm=1eb461e049c0f360a79e22ba9238b261cbcb5d604597e432e48bbc45891778c7&",
-    link: "https://clothes-shopping-page-responsive.vercel.app/",
+      "https://cdn.discordapp.com/attachments/1144637676016898219/1186345800658337852/Web_capture_18-12-2023_233422_mynt-fashion-blog.vercel.app.jpeg?ex=6592e9a0&is=658074a0&hm=f388a56c45afbe75c68cc4bc4e0526daee518ad52be6ec4e21cf29a42ae4d012&",
+    link: "https://mynt-fashion-blog.vercel.app/",
   },
 ];
 
