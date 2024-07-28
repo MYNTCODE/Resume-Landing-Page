1d006e37874c0543846f90a6381595adb0771006
mynt <myntpwork@gmail.com>
2024-07-28 19:37:19 +0700

    fix: education details

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import GoogleCertificate from "../assets/certificate/google-cer.jpg"
+import TechUpCretificate from "../assets/certificate/dev-cer.jpg"
 
 function Education() {
   return (
@@ -16,7 +18,7 @@ function Education() {
         <div className="flex flex-col lg:flex-row lg:gap-16 my-6 ">
           <div className="full-stack  h-[250px] lg:h-[650px] flex flex-col items-center w-[350px]">
             <p className=" text-xl font-semibold">
-              Full stack developer bootcamp
+              Full stack developer bootcamp (October 2023)
             </p>
             <p className=" lg:h-[200px]">
               Learn and practice about programming and create web application
@@ -30,12 +32,12 @@ function Education() {
             <div className="container my-4 border-4 border-gray-800 h-[80%] w-[250px]  rounded-2xl">
               <img
                 className="w-full h-full object-cover rounded-2xl"
-                src="https://cdn.discordapp.com/attachments/1144637676016898219/1173985747792035850/Screenshot_2023-11-14_205755.jpg?ex=6565f26d&is=65537d6d&hm=ba10559fb802f207a84a844e96fd7655e598cba0bf0c8b747002bb32d9682067&"
+                src={TechUpCretificate}
               ></img>
             </div>
 
             <a
-              href="https://cdn.discordapp.com/attachments/1144637676016898219/1173920045840216165/pornphimol_saelao_certificate.pdf?ex=6565b53d&is=6553403d&hm=4f0b23243d9b633713e9e05f7c83ce1d8e48289367829c615b486950e133c5a0&"
+              href="https://cdn.discordapp.com/attachments/1144637676016898219/1173920045840216165/pornphimol_saelao_certificate.pdf?ex=66a7633d&is=66a611bd&hm=8fff3db543dc1b8edc64b8f067fc59f995e65b4f166c2151251b11ecc2b4a953&"
               target="_blank"
               rel="noreferrer"
             >
@@ -45,7 +47,7 @@ function Education() {
             </a>
           </div>
           <div className="data  lg:h-[580px] flex flex-col items-center mt-[70%] lg:mt-0 md:mt-25 sm:mt-25 w-[350px]">
-            <p className=" text-xl font-semibold">Google data analytics </p>
+            <p className=" text-xl font-semibold">Google data analytics <br/>(April 2023)</p>
             <p className=" lg:h-[200px]">
               Learn strategic thinking skills about collecting data and using it
               correctly. Including the use of programming languages{" "}
@@ -54,7 +56,7 @@ function Education() {
             <div className="container my-4 border-4 border-gray-800 h-[80%] w-[250px]  rounded-2xl">
               <img
                 className="w-full h-full object-cover rounded-2xl"
-                src="https://cdn.discordapp.com/attachments/1144637676016898219/1173985350041997444/Screenshot_2023-11-14_205703.jpg?ex=6565f20f&is=65537d0f&hm=a4bdcdc4ab3d21deb9b130a6add31dab83d5ad3646aa47c0b3445ac0951c041d&"
+                src={GoogleCertificate}
               ></img>
               <a
                 href="https://coursera.org/share/7f61df63db25081e938de14cc952fb79"
